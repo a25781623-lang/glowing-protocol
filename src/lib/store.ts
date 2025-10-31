195bc433c62f5cb6c369e6a6577a7812857d9100
a25781623-lang <a25781623@gmail.com>
2025-10-31 16:07:03 +0530

    Update store.ts

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -7,12 +7,14 @@ interface SceneState {
   emissiveIntensity: number;
   cameraTarget: [number, number, number];
   reducedMotion: boolean;
+  meshScale: number; // Add this
   setHoveredNode: (id: number | null) => void;
   setSelectedNode: (id: number | null) => void;
   setLinkSpeed: (speed: number) => void;
   setEmissiveIntensity: (intensity: number) => void;
   setCameraTarget: (target: [number, number, number]) => void;
   setReducedMotion: (reduced: boolean) => void;
+  setMeshScale: (scale: number) => void; // Add this
 }
 
 export const useSceneStore = create<SceneState>((set) => ({
@@ -22,10 +24,12 @@ export const useSceneStore = create<SceneState>((set) => ({
   emissiveIntensity: 1.0,
   cameraTarget: [0, 0, 0],
   reducedMotion: false,
+  meshScale: 1.0, // Add this
   setHoveredNode: (id) => set({ hoveredNode: id }),
   setSelectedNode: (id) => set({ selectedNode: id }),
   setLinkSpeed: (speed) => set({ linkSpeed: speed }),
   setEmissiveIntensity: (intensity) => set({ emissiveIntensity: intensity }),
   setCameraTarget: (target) => set({ cameraTarget: target }),
   setReducedMotion: (reduced) => set({ reducedMotion: reduced }),
+  setMeshScale: (scale) => set({ meshScale: scale }), // Add this
 }));
